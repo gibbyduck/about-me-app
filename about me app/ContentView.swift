e9c6146acbe2c7459e4cde4a04d0cee389203f81
Genevieve Bills <gigicarlabills@gmail.com>
2023-06-08 13:23:46 -0500

    Initial Commit

---
+++ about me app/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  about me app
+//
+//  Created by Scholar on 6/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+HStack {
+    Text("✧info- gjkhgjahshbvckahbsbhdvsjvsvjvsdjvjdvvhjvdsjvjdvjvhjsdvjhdjhhjdvjvj bdsgjhsvdkjkdhshkd.")
+    Image("me")
+            .resizable()
+            .scaledToFit()
+    
+    }
+            
+HStack(spacing:235){
+    Text("✧")
+    Text("Genevieve Bills")
+        
+
+                }
+            
+            
+Image("blue gorl")
+                .resizable()
+                .scaledToFit()
+            
+Image("dazi")
+                .resizable()
+                .scaledToFit()
+            
+Image("pot")
+                .resizable()
+                .scaledToFit()
+            
+Image("alien")
+                .resizable()
+                .scaledToFit()
+                        
+    
+            
+            
+
+            
+         
+            
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
